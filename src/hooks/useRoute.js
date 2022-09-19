925af2f5b332eeab5066577f91fd5e7e8378fddb
wkdgusdnr55@gmail.com <wkdgusdnr55@gmail.com>
2022-09-19 20:20:42 +0900

    basic layout lint fix

--- src/hooks/useRoute.js
+++ src/hooks/useRoute.js
@@ -1,39 +1,18 @@
 //route 등록은 이곳에서 해주시면 됩니다.
-const pages = [
-    // {
-    //     path: "/sample",
-    //     title: "샘플",
-    //     icon: PicCenterOutlined,
-    //     isMenu: true,
-    //     childe: [
-    //         {
-    //             path: "/list",
-    //             title: "샘플 자식 리스트",
-    //             screen: SampleListIndex,
-    //             isMenu: true,
-    //         },
-    //         {
-    //             path: "/post/:id",
-    //             title: "샘플등록(pathvariable)",
-    //             screen: SampleDetail,
-    //             isMenu: true,
-    //         },
-    //     ],
-    // },
-];
+const pages = [{}];
+
 export const useCustomRoute = () => {
     const menus = pages?.filter((v) => !!v.isMenu);
     const routes = getAllPath(pages);
     return { routes, menus };
 };
-
 const getAllPath = (child = []) => {
     const result = [];
 
     child?.forEach((item) => {
         result.push(item);
-        if (item?.childe) {
-            const childrens = getAllPath(item?.childe).map((el) => ({
+        if (item?.child) {
+            const childrens = getAllPath(item?.child).map((el) => ({
                 ...el,
                 path: `${item.path}${el.path}`,
             }));
