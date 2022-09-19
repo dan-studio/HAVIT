387b1853457a879e5e2ea8576c51ff87ebf5a609
wildgallop21 <wildgallop21@gmail.com>
2022-09-19 18:50:07 +0900

    파일명에서 :제거

--- src/hooks/useRoute.js
+++ src/hooks/useRoute.js
@@ -1,3 +1,7 @@
+import Detail from "../pages/Detail";
+import Group from "../pages/Group";
+import Write from "../pages/Write";
+
 //route 등록은 이곳에서 해주시면 됩니다.
 const pages = [
     // {
@@ -20,6 +24,31 @@ const pages = [
     //         },
     //     ],
     // },
+        {
+        path: "/group",
+        title: "샘플",
+        screen: Group,
+        icon: "",
+        isMenu: true,
+        childe: [
+            {
+                path: "/detail",
+                title: "샘플 자식 리스트",
+                screen: Detail,
+                isMenu: true,
+                childe: [
+                    {
+                        path: "/write",
+                        title: "샘플등록(pathvariable)",
+                        screen: Write,
+                        isMenu: true,
+                    },
+                ]
+
+            }
+
+        ],
+    },
 ];
 export const useCustomRoute = () => {
     const menus = pages?.filter((v) => !!v.isMenu);
---
+++ src/pages/Detail.jsx
@@ -0,0 +1,9 @@
+const Detail = () => {
+
+
+  return (
+    <div>detail</div>
+  )
+}
+
+export default Detail
