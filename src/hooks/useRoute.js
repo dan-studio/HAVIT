22de114d18d1564bb6fef33698f646aeb07382b0
미니 <83333217+MIINII@users.noreply.github.com>
2022-09-20 15:10:57 +0900

    :wrench: [ROUTE] 라우트 수정

--- src/hooks/useRoute.js
+++ src/hooks/useRoute.js
@@ -7,32 +7,38 @@ import MypageEdit from './../pages/MypageEdit';
 //route 등록은 이곳에서 해주시면 됩니다.
 
 const pages = [
-    {
-        path: "/group",
-        title: "샘플",
-        screen: Group,
-        icon: "",
+  {
+    path: '/group',
+    title: '샘플',
+    screen: Group,
+    icon: '',
+    isMenu: true,
+    childe: [
+      {
+        path: '/detail',
+        title: '샘플 자식 리스트',
+        screen: Detail,
         isMenu: true,
         childe: [
-            {
-                path: "/detail",
-                title: "샘플 자식 리스트",
-                screen: Detail,
+          {
+            path: '/detail',
+            title: '샘플 자식 리스트',
+            screen: Detail,
+            isMenu: true,
+            childe: [
+              {
+                path: '/write',
+                title: '샘플등록(pathvariable)',
+                screen: Write,
                 isMenu: true,
-                childe: [
-                    {
-                        path: "/write",
-                        title: "샘플등록(pathvariable)",
-                        screen: Write,
-                        isMenu: true,
-                    },
-                ],
-            },
-
+              },
+            ],
+          },
         ],
       },
     ],
   },
+
   {
     path: '/mypage',
     title: '마이페이지',
@@ -57,16 +63,15 @@ export const useCustomRoute = () => {
 const getAllPath = (child = []) => {
   const result = [];
 
-
-    child?.forEach((item) => {
-        result.push(item);
-        if (item?.child) {
-            const childrens = getAllPath(item?.child).map((el) => ({
-                ...el,
-                path: `${item.path}${el.path}`,
-            }));
-            result.push(...childrens);
-        }
-    });
-    return result;
+  child?.forEach(item => {
+    result.push(item);
+    if (item?.child) {
+      const childrens = getAllPath(item?.child).map(el => ({
+        ...el,
+        path: `${item.path}${el.path}`,
+      }));
+      result.push(...childrens);
+    }
+  });
+  return result;
 };
