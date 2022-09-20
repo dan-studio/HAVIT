0f4eedc6d1ba051901f234c9515195d7a2b9c6cf
dan-studio <danstudiocode@gmail.com>
2022-09-20 14:52:51 +0900

    Feature: 로그인, 회원가입, 404페이지 UI 완료

--- src/hooks/useRoute.js
+++ src/hooks/useRoute.js
@@ -1,8 +1,11 @@
+import Splash from "../pages/Splash";
 import Start from "../pages/Start";
-import Login from "../pages/Login";
 import Detail from "../pages/Detail";
 import Group from "../pages/Group";
 import Write from "../pages/Write";
+import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
+import PageNotFound from "../pages/PageNotFound";
 
 //route 등록은 이곳에서 해주시면 됩니다.
 
@@ -11,14 +14,35 @@ const pages = [
     path: "/splash",
     title: "스플래쉬",
     icon: "",
+    screen: Splash,
+    isMenu: true,
+  },
+  {
+    path: "/pagenotfound",
+    title: "404",
+    icon: "",
+    screen: PageNotFound,
+    isMenu: true,
+  },
+  {
+    path: "/startpage",
+    title: "시작 페이지",
+    icon: "",
     screen: Start,
     isMenu: true,
   },
   {
-    path: "/login",
+    path: "/signin",
     title: "로그인",
     icon: "",
-    screen: Login,
+    screen: Signin,
+    isMenu: true,
+  },
+  {
+    path: "/signup",
+    title: "회원가입",
+    icon: "",
+    screen: Signup,
     isMenu: true,
   },
   {
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,59 +1,64 @@
-import React, { useEffect } from "react";
-import styled from "styled-components";
-import logo from "../assets/havit_black.svg";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import havit from '../assets/havitLogoPurple.png'
 
-const Start = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    setTimeout(() => {
-      navigate("/login");
-    }, 2000);
-  }, );
+const Login = () => {
+
+const navigate = useNavigate()
   return (
     <StyledDiv>
-      <StyledImg src={logo} />
-      <StyledP>혼자가 아닌, 함께 만들어가는 습관.</StyledP>
-      <StyledSpinner />
+      <StyledSpan>
+        환영해요 <br />
+        <img
+          src={havit}
+          alt=""
+        />{" "}
+        &emsp;&emsp;&emsp;&emsp;&ensp;&nbsp; 은 <br />
+        처음이신가요?
+      </StyledSpan>
+
+      <StyledButtonDiv>
+        <StyledButton top="65vh" color="white" background="#5C53FF" onClick={()=>{navigate('/signup')}}>
+          네, 처음이에요
+        </StyledButton>
+        <StyledButton top="71vh" background="white" onClick={()=>{navigate('/signin')}}>
+          아니요, 이미 회원이에요
+        </StyledButton>
+      </StyledButtonDiv>
     </StyledDiv>
   );
 };
 
-export default Start;
+export default Login;
 
-const StyledDiv = styled.div`
-  height: 80vh;
+const StyledDiv = styled.div``;
+const StyledSpan = styled.span`
+  color: #252224;
+  position: absolute;
+  left: 15vw;
+  top: 25vh;
+  font-weight: 400;
+  font-size: 35px;
+  line-height: 50px;
+  img {
+    position: absolute;
+    margin-top: .5vh;
+  }
+`;
+
+const StyledButtonDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 `;
-const StyledImg = styled.img`
-  width: 200px;
-`;
-const StyledP = styled.p`
-  margin-top: 35vh;
+const StyledButton = styled.button`
   position: absolute;
-  font-size: 16px;
-  text-align: center;
-  color: #575757;
-`;
-const StyledSpinner = styled.div`
-  box-sizing: border-box;
-  position: absolute;
-  top: 65vh;
-  left: 50%;
-  width: 64px;
-  height: 64px;
-  margin-top: -32px;
-  margin-left: -32px;
-  border-radius: 50%;
-  border: 8px solid transparent;
-  border-top-color: #575757;
-  border-bottom-color: #575757;
-  animation: spinner .9s ease infinite;
-
-  @keyframes spinner {
-  from {transform: rotate(0deg); }
-  to {transform: rotate(360deg);}
-}
+  top: ${(props) => props.top};
+  width: 70vw;
+  color: ${(props) => props.color};
+  border: 1px solid #5c53ff;
+  background-color: ${(props) => props.background};
+  padding: 5px;
+  border-radius: 30px;
 `;
