95fea9e6986ca83f1264a883b153569a58b877c0
MIINII <83333217+MIINII@users.noreply.github.com>
2022-09-22 15:08:03 +0900

    :sparkles: [EDIT] add 'fature/pages/edit'

--- src/pages/MypageEdit.jsx
+++ src/pages/MypageEdit.jsx
@@ -1,19 +1,37 @@
-import { useDispatch } from "react-redux";
-import { setLayout } from "@redux/layout";
-import { useEffect } from "react";
-import { resetLayout } from "../redux/layout";
+import { useDispatch } from 'react-redux';
+import { setLayout } from '@redux/layout';
+import { useEffect } from 'react';
+import { resetLayout } from '../redux/layout';
+import styled from 'styled-components';
 
 const MypageEdit = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(setLayout({ showHeader: false }));
-        return () => {
-            dispatch(resetLayout());
-        };
-    }, []);
+  useEffect(() => {
+    dispatch(setLayout({ showHeader: false }));
+    return () => {
+      dispatch(resetLayout());
+    };
+  }, []);
 
-    return <>sdsd</>;
+  return (
+    <>
+      <UserImg/>
+    </>
+  );
 };
 
+const UserImg = styled.div`
+  width: 170px;
+  height: 170px;
+  margin-right: 20px;
+  background-color: red;
+  border-radius: 100%;
+  overflow: hidden;
+  & > img {
+    width: 92px;
+    height: 92px;
+    object-fit: cover;
+  }
+`;
 export default MypageEdit;
