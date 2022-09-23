11082179300805f784c734f6ef2737333ca19ee7
dan-studio <danstudiocode@gmail.com>
2022-09-23 15:58:13 +0900

    Feature: 로그인시 token 값 넣음

--- src/components/editprofile/EditInput.jsx
+++ src/components/editprofile/EditInput.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { RiPencilFill } from 'react-icons/ri';
 import { Button } from 'antd';
 
-const EditInput = ({ inputLabel, type }) => {
+const EditInput = ({ inputLabel, type, onChange}) => {
   const [inputValue, setInputValue] = React.useState('');
 
   return (
@@ -11,7 +11,7 @@ const EditInput = ({ inputLabel, type }) => {
       <label style={{ fontWeight: '400', fontSize: '16px', lineHeight: '24px', marginBottom: '10px' }} htmlFor='userEditInput'>
         {inputLabel}
       </label>
-      <input type={type} id='userEditInput' placeholder={inputValue} />
+      <input type={type} id='userEditInput' placeholder={inputValue} onChange={onChange}/>
     </Wrap>
   );
 };
@@ -25,7 +25,7 @@ const Wrap = styled.div`
     padding: 10px;
     background: #ffffff;
     border: 0.5px solid #eaeaea;
-    border-radius: 10px;
+    border-radius: 30px;
   }
 `;
 
