74c39f8df2176561f82d5614edd90b32e927cab8
wildgallop21 <wildgallop21@gmail.com>
2022-09-24 02:31:47 +0900

    write페이지 업로더 반영안된버전

---
+++ src/components/layout/Location.jsx
@@ -0,0 +1,113 @@
+import { SearchOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+
+const Location = ({ locationRef }) => {
+  const locationDisable = useRef();
+
+  return (
+    <Cover onClick={locationDisable}>
+      <Container ref={locationRef}>
+        <SearchBox>
+          <Input
+            type='search'
+            style={{
+              width: '90%',
+              height: '3rem',
+              borderRadius: '25px',
+            }}
+            prefix={
+              <SearchOutlined
+                style={{
+                  color: '#5E43FF',
+                  fontSize: '1.25rem',
+                  fontWeight: 'bold',
+                }}></SearchOutlined>
+            }></Input>
+        </SearchBox>
+        <HistoryBox>
+          <h2>최근위치검색기록</h2>
+          <div>스파르타</div>
+          <div>용용선생</div>
+          <div>서울대학교</div>
+          <div>김병처리 집</div>
+
+        </HistoryBox>
+        <DragLine></DragLine>
+      </Container>
+    </Cover>
+  );
+};
+
+export default Location;
+
+const Cover = styled.div`
+  background-color: rgba(0, 0, 0, 0.75);
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+`;
+const Container = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 80%;
+  background-color: white;
+  border-bottom-left-radius: 2rem;
+  border-bottom-right-radius: 2rem;
+  animation: 500ms dropdown forwards 1;
+  top: -100%;
+  left: 0;
+  @keyframes dropdown {
+    from {
+      top: -100%;
+    }
+    to {
+      top: 0%;
+    }
+  }
+`;
+const SearchBox = styled.div`
+  width: 100%;
+  height: 128px;
+  margin: 2 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const HistoryBox = styled.div`
+  width: 100%;
+  max-height: 70%;
+  padding: 0 2.5rem;
+  overflow: hidden;
+  & > h2 {
+    font-size: 1.125rem;
+    font-weight: bold;
+    letter-spacing: -2px;
+    color: ${({ theme }) => {
+      console.log(theme.color.neongreen);
+      return theme.color.neongreen;
+    }};
+    margin-bottom: 1rem;
+  }
+  & > div {
+    font-size: 1rem;
+    font-weight: 600;
+    margin: 0.25rem 0;
+  }
+`;
+const DragLine = styled.div`
+  position: absolute;
+  bottom: 25px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 40%;
+  height: 4px;
+  border-radius: 25px;
+  background-color: ${({ theme }) => {
+    return theme.color.black;
+  }};
+`;
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,109 +1,130 @@
 import styled from "styled-components";
-import React from "react";
-import { AiOutlinePlusCircle} from "react-icons/ai";
-import Button from "../components/PrimaryButton";
+import React, { useEffect, useState } from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import PrimaryButton from "../components/PrimaryButton";
+import SubButton from "../components/SubButton";
+import Location from "../components/layout/Location";
+import Uploader from "../input/Uploader";
+import { Upload } from "antd";
 
 
+import { IoLocationOutline } from 'react-icons/io5';
 
 const Write = () => {
+  const register = console.log("등록완료");
+  const locationRef = React.useRef();
 
+  const [showLocationForm, setShowLocationForm] = useState(false);
+  useEffect(() => {
+    let handler = (e) => {
+      if (!locationRef.current?.contains(e.target)) {
+        setShowLocationForm(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
   return (
     <div>
-      <WriteBox>        
-      <AddPhoto>
-        <IconBox>
-        <AiOutlinePlusCircle size="60" color="lightgray"/>
-        </IconBox>
-      </AddPhoto>
-      <InputTitle placeholder= "제목입력">
-      
-      </InputTitle>
-
-      <AddLocation>
-      {/* <LibraryAddIcon /> */}
-      위치추가
-      </AddLocation>
-      <WriteBtn>
-        <PostBtn>등록</PostBtn>
-        <CancelBtn>취소</CancelBtn>
-      </WriteBtn>
+      <WriteBox>
+        <AddPhoto>
+          <IconBox>
+            <AiOutlinePlusCircle size="60" color="lightgray" />
+            {/* <Uploader /> */}
+          </IconBox>
+        </AddPhoto>
+        <div style={{display:'flex', flexDirection:'column', justifyContent:"space-between", alignItems:'center'}}>
+          <InputTitle placeholder="제목입력" />
+  
+          <AddLocation style={{marginTop:'10px', display:'flex', alignItems:'center',}} onClick={() => setShowLocationForm(true)}>
+            {/* <LibraryAddIcon /> */}
+            <IoLocationOutline style={{fontSize:'18px', color:'#DE4242', marginRight:'5px'}}/>
+            위치추가
+          </AddLocation>
+        </div>
+        {!!showLocationForm ? <Location /> : <> </>}
+        <WriteBtn style={{marginTop:'5.625rem'}}>
+          {/* <PostBtn>등록</PostBtn>
+        <CancelBtn>취소</CancelBtn> */}
+          <PrimaryButton buttonName={"등록"} onClick={register} />
+          <SubButton buttonName={"취소"} />
+        </WriteBtn>
       </WriteBox>
-
     </div>
-  )
-}
-
+  );
+};
 
 const WriteBox = styled.div`
-display:flex;
-flex-direction: column;
-/* @media screen and (max-width: 900px) {
+  display: flex;
+  flex-direction: column;
+  /* @media screen and (max-width: 900px) {
   article {
     padding: 1rem 3rem;
   }
 }
 background-color:blue; */
-width:390px;
-height:844px;
-margin:0 auto;
+  width: 390px;
+  height: 844px;
+  margin: 0 auto;
 `;
 const AddPhoto = styled.div`
-width :350px;
-height:350px;
-/* background-color:pink; */
-border: 0.5px solid #eaeaea;
+  width: 350px;
+  height: 350px;
+  /* background-color:pink; */
+  border: 0.5px solid #eaeaea;
   background-color: #ffffff;
 
-margin: 0 auto;
-
+  margin: 0 auto;
 `;
 const InputTitle = styled.input`
-width:334px;
-border-color:  transparent transparent gray transparent;
-margin: 10px 10px;
-
+  width: 334px;
+  border-color: transparent transparent gray transparent;
+  margin: 100px auto 0 auto;
 `;
 const IconBox = styled.div`
-/* position: center; */
-margin: 160px 160px 160px 160px;
-
+  /* position: center; */
+  margin: 160px 160px 160px 160px;
 `;
 
 const AddLocation = styled.button`
-width:80px;
-border-color:transparent;
-background-color:transparent;
-margin-right: 0;
-
+  width: 350px;
+  border-color: transparent;
+  background-color: transparent;
+  margin-right: 0;
+  font-weight: bold;
+  font-size: 15px;
+  margin-top: 10px 20px 0 20px;
+  text-align: left;
 `;
 const WriteBtn = styled.div`
-display:flex;
-flex-direction:row;
-/* position: center; */
-margin: 0 auto;
-
+  display: flex;
+  flex-direction: row;
+  /* position: center; */
+  margin: 0 auto;
 `;
 const PostBtn = styled.button`
-margin: 50px 0 50px 0; 
-border-radius: 25px ;
-width:50px;
-height:30px;
-border-color:transparent;
-background-color:#5e43ff;
-color:white;
-font-size: small
+  margin: 50px 0 50px 0;
+  border-radius: 25px;
+  width: 50px;
+  height: 30px;
+  border-color: transparent;
+  background-color: #5e43ff;
+  color: white;
+  font-size: small;
 `;
 
 const CancelBtn = styled.button`
-margin: 50px 0 50px 0; 
-border-radius: 25px ;
-width:50px;
-height:30px;
-border-color:transparent;
-background-color: transparent;
-color:#5e43ff;
-font-size: small
+  margin: 50px 0 50px 0;
+  border-radius: 25px;
+  width: 50px;
+  height: 30px;
+  border-color: transparent;
+  background-color: transparent;
+  color: #5e43ff;
+  font-size: small;
 `;
 
-export default Write
+export default Write;
