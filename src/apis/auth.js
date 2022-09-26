adcb99a22b30945ba278c80dc4f1459766033aaf
미니 <83333217+MIINII@users.noreply.github.com>
2022-09-26 15:50:52 +0900

    [APIS] config.js 재정렬 / auth.js userInfo, editProfile 추가

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,30 +1,35 @@
-import { authApi, mockApi, restApi } from "./config";
+import { authApi, mockApi, restApi } from './config';
 
 export const userApis = {
-  signup: async (data) => {
-    const response = await restApi.post("/api/signup", data);
+  signup: async data => {
+    const response = await restApi.post('/api/signup', data);
     return response.data;
   },
 
   signin: async (email, password) => {
-    const response = await authApi.post("/api/login", {
+    const response = await authApi.post('/api/login', {
       email,
       password,
     });
     return response;
   },
 
+  usersInfo: async (nickname, profileUrl, crew) => {
+    const response = await mockApi.get('/users', { nickname, profileUrl, crew });
+    return response;
+  },
+
   editProfile: async (nickname, password, passwordConfirm) => {
-    const response = await authApi.post('/api/auth/mypage', {
+    const response = await mockApi.post('/users', {
       nickname,
       password,
       passwordConfirm,
     });
     return response.data;
   },
-  
-  getgroup: async () => {
-    const response = await authApi.get("/api/group");
+
+  getgroup: async data => {
+    const response = await mockApi.get('/group', data);
     return response;
   },
 };
