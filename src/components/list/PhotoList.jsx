4164a150d89a68b9805cc452d916e7a20999098d
wildgallop21 <wildgallop21@gmail.com>
2022-09-30 00:51:20 +0900

    챌린지 제목, 사진, 그룹아이디 post 구현

--- src/components/list/PhotoList.jsx
+++ src/components/list/PhotoList.jsx
@@ -1,25 +1,54 @@
 import { PlusCircleFilled } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { userApis } from "../../apis/auth";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const PhotoList = ({groupId}) => {
     const navigate = useNavigate()
+    const [certifyImg, setCertifyImg] = useState();
+
+    useEffect(() => {
+        userApis
+          .getCertify(groupId)
+          .then((res) => {
+            console.log(res)
+            setCertifyImg(res.data.data.certifyImgUrlList);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, []);
+    console.log("certifyImg",certifyImg)
+    const imgMap = certifyImg?.map((el) => el)
+    // const imgMap = certifyImg?.map((item, idx) => <CertifyImg {...item} key={idx} />)
+    // {group?.map((item, idx) => ( <CrewInfo {...item} key={idx} /> ))}
+      console.log(imgMap)
     return (
-        <Container>
+        <div>
+        <StyledContainer>
             <div>
                 <PlusCircleFilled style={{ color: "#eeeeee" }} onClick={()=>{navigate(`/group/${groupId}/write`)}}/>
             </div>
+            {certifyImg?.map((item, idx) => <CertifyImg src= {item} key={idx} />)}
+            {/* <CertifyImg src={imgMap}></CertifyImg> */}
+            {/* // src= ["sdfasfsdf"] */}
+            {/* <CertifyImg src={certifyImg?.[1]}></CertifyImg>
+            <CertifyImg src={certifyImg?.[3]}></CertifyImg> */}
+
             <div></div>
             <div></div>
             <div></div>
             <div></div>
             <div></div>
-        </Container>
+        </StyledContainer>
+        </div>
     );
 };
 export default PhotoList;
 
-const Container = styled.div`
+const StyledContainer = styled.div`
     width: 100%;
     gap: 5px;
     display: flex;
@@ -36,3 +65,9 @@ const Container = styled.div`
         font-size: 2rem;
     }
 `;
+const CertifyImg = styled.img`
+        width: 113px;
+        height: 113px;
+        border: 0.5px solid #eeeeee;
+`
+
