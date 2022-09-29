3cecc64b845880c0bb14040f3bb6ea6ea2b0e251
MIINII <83333217+MIINII@users.noreply.github.com>
2022-09-29 16:42:21 +0900

    [APIS] 비밀번호 비교할수있는 API 추가

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -18,44 +18,44 @@ export const userApis = {
     return response;
   },
 
-  userProfile: async data => {
-    const reponse = await restApi.get('api/mypage', data);
-    return reponse.data;
-  },
   //Group
   getGroup: async () => {
-    const response = await authApi.get("/api/auth/group/");
+    const response = await authApi.get('/api/auth/group/');
     return response;
   },
 
-  getGroupDetail: async (id) => {
-    const response = await authApi.get("/api/auth/group/"+id);
-    return response;
-  },
-  joinGroup: async (data) => {
-    const response = await authApi.post("api/auth/participate/"+data)
-    return response
-  },
-  leaveGroup: async (data) => {
-    const response = await authApi.delete("api/auth/participate/"+data)
-    return response
+  getGroupDetail: async id => {
+    // const response = await mockApi.get("/api/auth/group/"+id);
+    const response = await mockApi.get('/group');
+    console.log(response);
+    const detail = response.data.find(item => item.groupId === id);
+    return detail;
   },
 
   uploadImage: async data => {
     const response = await authApi.post('/api/auth/certify/', data);
+    console.log('🚀 ⁝ response', response);
     return response;
   },
 
   // 마이페이지 내에서의 내 정보
-  userProfile: async () => {
-    const reponse = await authApi.get('/api/auth/mypage/');
-    console.log('🚀 ⁝ userProfile: ⁝ reponse', reponse)
-    return reponse;
-  },
+  // userProfile: async data => {
+  //   const reponse = await authApi.get('/users');
+  //   console.log('🚀 ⁝ userProfile: ⁝ reponse', reponse);
+  //   return reponse;
+  // },
 
   // 마이페이지 내에서의 유저 정보
-  usersInfo: async (nickname, profileUrl, crew, email) => {
-    const response = await authApi.get('/api/auth/main', { nickname, profileUrl, crew, email });
+  // usersInfo: async (nickname, profileUrl, crew, email) => {
+  //   const response = await authApi.get('/api/auth/main', { nickname, profileUrl, crew, email });
+  //   return response;
+  // },
+
+  // 마이페이지에서 비밀번호 확인하기
+  userPwCheck: async password => {
+    const response = await authApi.post('/api/auth/mypage/check', { password });
+    console.log('🚀 ⁝ password', typeof password);
+
     return response;
   },
 };
